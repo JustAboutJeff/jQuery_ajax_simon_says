87520188532e738d2d8a51c5b1e20a2a50fd0026
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 11:14:38 -0500

    Random color and tile selection in place

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+  $('a#get_color').on('click', function() {
+    var randomTile = Math.round(Math.random()*8);
+    var randomHue = '#' + ('00000' + Math.floor(Math.random()*16777216).toString(16)).substr(-6)
 
+
+    $("ul#color_me > li").eq(randomTile).css('background-color', randomHue);
+  })
 });
